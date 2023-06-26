ca8003ba940f04cf7372e7240beef69eae2f013b
ashaduzzaman2002 <ashaduzzaman2002@gmail.com>
2023-06-26 19:24:45 +0530

    update

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -20,8 +20,6 @@ const Home = () => {
       style={{
         backgroundColor: '#08224e',
         minHeight: '100vh',
-        marginBottom: '-30px',
-        marginTop: '-2rem',
         paddingTop: '2rem',
       }}
     >
@@ -30,25 +28,6 @@ const Home = () => {
 
         <div className="home-container">
           <div className="home-top">
-            {/* <h4 style={{ paddingTop: '2rem' }}>Points</h4> */}
-
-            {/* <div className="home-top-item">
-              <div>
-                <p>
-                  INR <span>0.0</span>
-                </p>
-                <p>UID: 0</p>
-              </div>
-
-              <div className="home-profile"></div>
-            </div> */}
-
-            {/* <div className="home-card1-group">
-            <Card1 url='/withdraw' icon={Money} text="withdraw" />
-            <Card1 url='/recharge' icon={vault} text="recharge" />
-            <Card1 url='/check-in' icon={check} text="Check in" />
-            <Card1 url={'/rewards'} icon={reward} text="Reward" />
-          </div> */}
 
             <div class="balance__section">
               <div class="balance__section__left">
