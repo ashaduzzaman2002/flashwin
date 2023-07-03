a437f0bfe904204b38b4eda8529e9907a1d028d7
vivek <vivekdgp01@gmail.com>
2023-07-03 09:31:15 +0530

    API Integrated

--- src/screens/checkin/CheckIn.jsx
+++ src/screens/checkin/CheckIn.jsx
@@ -3,19 +3,116 @@ import './CheckIn.css';
 import { coin, treasure } from '../../assets';
 import { dbObject } from '../../helper/constant';
 
+import { toast, ToastContainer } from 'react-toastify';
+
 const CheckIn = () => {
   const dayList = [1, 2, 3, 4, 5, 6, 7];
 
-  const getData = async() => {
-    // const {data} = await dbObject('/task/checkin')
-    // console.log(data);
+  const checkinData = [
+    {
+      'label': 'Day 1',
+      'amount': 1,
+      'icon': 'coin',
+      'listLabel': 'day1',
+    },
+    {
+      'label': 'Day 2',
+      'amount': 2,
+      'icon': 'coin',
+      'listLabel': 'day2',
+    },
+    {
+      'label': 'Day 3',
+      'amount': 2,
+      'icon': 'coin',
+      'listLabel': 'day3',
+    },
+    {
+      'label': 'Day 4',
+      'amount': 2,
+      'icon': 'coin',
+      'listLabel': 'day4',
+    },
+    {
+      'label': 'Day 5',
+      'amount': 3,
+      'icon': 'coin',
+      'listLabel': 'day5',
+    },
+    {
+      'label': 'Day 6',
+      'amount': 3,
+      'icon': 'coin',
+      'listLabel': 'day6',
+    },
+    {
+      'label': 'Day 7',
+      'amount': 3,
+      'icon': 'coin',
+      'listLabel': 'day7',
+    },
+  ];
+
+  let dailyCheckinList = {
+    "day1": null,
+    "day2": null,
+    "day3": null,
+    "day4": null,
+    "day5": null,
+    "day6": null,
+    "day7": null
+  };
+
+  const fetchCheckinDetails = async () => {
+    try {
+      const { data } = await dbObject.post('/task/checkin/fetch')
+      // console.log(data.error);
+      if (!data.error) {
+        dailyCheckinList = data.data[0];
+        // console.log(dailyCheckinList.day1);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
-  
+
+  const claimCheckinReward = async () => {
+    try {
+      const { data } = await dbObject.post("/task/checkin");
+      // console.log(data);
+      if (!data.error) {
+        toast.success(data.message, {
+          position: 'top-center',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'colored',
+        });
+      } else {
+        toast.error(data.message, {
+          position: 'top-center',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'colored',
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
-   getData()
+    fetchCheckinDetails();
   }, [])
-  
-  
+
+
   return (
     <div
       style={{
@@ -37,16 +134,29 @@ const CheckIn = () => {
           </div>
 
           <div>
-            <button className="checkin-btn">Check In</button>
+            <button className="checkin-btn" onClick={claimCheckinReward}>Check In</button>
           </div>
 
           <p className='checkin-desc'>Check in for 7 consecutive days to get the key for mega tresure box and recive the mysterious prizes!</p>
 
-            <div className='checkin-tresure'>
+          <div className='checkin-tresure'>
             <img src={treasure} alt="" />
-            <div  />
-            </div>
-          
+            <div />
+          </div>
+
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+          />
+
         </div>
       </div>
     </div>
--- src/screens/rewards/Rewards.jsx
+++ src/screens/rewards/Rewards.jsx
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Rewards.css'
 import Progressbar from '../../components/ProgressBar';
 import { profileFilled, receipt } from '../../assets';
+import { dbObject } from '../../helper/constant';
 
 const Rewards = () => {
+
+  let taskRewardList = {
+    'task1': {
+      'task': 'Invite first customer',
+      'reward': 5,
+      'target': 1,
+      'achieved': 0
+    },
+    'task2': {
+      'task': 'Invite customer with your refer and make them first recharge',
+      'reward': 20,
+      'target': 1,
+      'achieved': 0
+    },
+    'task3': {
+      'task': 'Do your first recharge',
+      'reward': 20,
+      'target': 1,
+      'achieved': 0
+    },
+    'task4': {
+      'task': 'Play 100 games',
+      'reward': 15,
+      'target': 100,
+      'achieved': 0
+    },
+    'task5': {
+      'task': 'Play 1000 games',
+      'reward': 40,
+      'target': 1000,
+      'achieved': 0
+    },
+    'task6': {
+      'task': 'Play 10000 games',
+      'reward': 300,
+      'target': 10000,
+      'achieved': 0
+    }
+  };
+
+
+  const fetchTaskRewardDetails = async () => {
+    try {
+      const { data } = await dbObject.get("/task/reward");
+      // console.log(data.task1);
+      if (!data.error) {
+        taskRewardList.task1 = data.task1;
+        taskRewardList.task2 = data.task2;
+        taskRewardList.task3 = data.task3;
+        taskRewardList.task4 = data.task4;
+        taskRewardList.task5 = data.task5;
+        taskRewardList.task6 = data.task6;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  useEffect(() => {
+    fetchTaskRewardDetails();
+  }, [])
+
+
   return (
     <div
       style={{
@@ -16,11 +81,10 @@ const Rewards = () => {
           <h1>Check In</h1>
 
           <div className='reward-card-group'>
-          <RewardCard bgcolor={'#62ffdb'} icon={profileFilled} progressRate='0' btn={'Invite'} />
-          <RewardCard bgcolor={'#ffc008'} icon={profileFilled} progressRate='0' btn={'Invite'} />
-          <RewardCard bgcolor={'#62ffdb'} icon={profileFilled} progressRate='50' btn={'Invite'} />
-          <RewardCard bgcolor={'#ffc008'} icon={profileFilled} progressRate='50' btn={'Invite'} />
-
+            <RewardCard bgcolor={'#62ffdb'} icon={profileFilled} progressRate='0' btn={'Invite'} />
+            <RewardCard bgcolor={'#ffc008'} icon={profileFilled} progressRate='0' btn={'Invite'} />
+            <RewardCard bgcolor={'#62ffdb'} icon={profileFilled} progressRate='50' btn={'Invite'} />
+            <RewardCard bgcolor={'#ffc008'} icon={profileFilled} progressRate='50' btn={'Invite'} />
           </div>
         </div>
       </div>
@@ -28,32 +92,33 @@ const Rewards = () => {
   );
 };
 
-const RewardCard = ({progressRate, btn, bgcolor, icon}) => {
-  console.log(progressRate);
+const RewardCard = ({ progressRate, btn, bgcolor, icon }) => {
+  // console.log(progressRate);
   return (
-  <div className='reward-card'>
-    <div className='reward-card-desc'>
-      <img style={{backgroundColor: bgcolor}} src={icon} alt="" />
+    <div className='reward-card'>
+      <div className='reward-card-desc'>
+        <img style={{ backgroundColor: bgcolor }} src={icon} alt="" />
 
-      <div>
-        <h3>First Invitation</h3>
-        <p>After invitation users to complete download registration and purchase points, they can recive</p>
+        <div>
+          <h3>First Invitation</h3>
+          <p>After invitation users to complete download registration and purchase points, they can recive</p>
+        </div>
+
+        <h2>+5.0</h2>
       </div>
 
-      <h2>+5.0</h2>
-    </div>
-    
-    <Progressbar bgcolor="#69f0ae" progress={progressRate}  height={6} />
-    <div className='progress-percentage'>
-      <p>{progressRate}%</p>
-      <p>100%</p>
-    </div>
+      <Progressbar bgcolor="#69f0ae" progress={progressRate} height={6} />
+      <div className='progress-percentage'>
+        <p>{progressRate}%</p>
+        <p>100%</p>
+      </div>
 
-    <div>
-          <button className='reward-btn'>{btn}</button>
-        </div>
-  </div>
-)}
+      <div>
+        <button className='reward-btn'>{btn}</button>
+      </div>
+    </div>
+  )
+}
 
 
 export default Rewards;
