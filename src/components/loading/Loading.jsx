88281afe5fc9eeb98a9c017be8cefd48da8aeff0
ashaduzzaman2002 <ashaduzzaman2002@gmail.com>
2023-07-22 22:14:35 +0530

    add loading

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './loading.css';
+
+const Loading = () => {
+  return (
+    <div className='loading-container'>
+      <div class="loader"></div>
+    </div>
+  );
+};
+
+export default Loading;
--- src/screens/auth/Login.jsx
+++ src/screens/auth/Login.jsx
@@ -7,6 +7,7 @@ import { useFormik } from 'formik';
 import { loginValidation } from '../../validation';
 import { dbObject } from '../../helper/constant';
 import { AuthContext } from '../../context/AuthContext';
+import Loading from '../../components/loading/Loading';
 
 const initialValues = {
   number: '',
@@ -28,6 +29,8 @@ const Login = () => {
       }
     }
   }, [user]);
+
+
 
   const { values, errors, handleBlur, touched, handleChange, handleSubmit } =
     useFormik({
@@ -71,92 +74,98 @@ const Login = () => {
       },
     });
 
-  return (
-    <div className="auth-container">
-      <h2>
-        Log <span style={{ color: '#67efaf' }}>In</span>{' '}
-      </h2>
-      <form onSubmit={handleSubmit} className="auth-form " action="">
-        <div style={{ marginBottom: '1.5rem' }}>
-          <label htmlFor="number">Phone</label>
-          <div className="auth-input phone-input">
-            <i className="fa-solid fa-mobile-screen-button"></i>
-            <p>+91</p>
-            <input
-              value={values.number}
-              onChange={handleChange}
-              id="number"
-              autoComplete="off"
-              type="number"
-              placeholder="8909XXXXXX"
-              name="number"
-              onBlur={handleBlur}
-            />
+  if(loading) {
+    return <Loading />
+  }else {
+    return (
+      <div className="auth-container">
+        <h2>
+          Log <span style={{ color: '#67efaf' }}>In</span>{' '}
+        </h2>
+        <form onSubmit={handleSubmit} className="auth-form " action="">
+          <div style={{ marginBottom: '1.5rem' }}>
+            <label htmlFor="number">Phone</label>
+            <div className="auth-input phone-input">
+              <i className="fa-solid fa-mobile-screen-button"></i>
+              <p>+91</p>
+              <input
+                value={values.number}
+                onChange={handleChange}
+                id="number"
+                autoComplete="off"
+                type="number"
+                placeholder="8909XXXXXX"
+                name="number"
+                onBlur={handleBlur}
+              />
+            </div>
+  
+            {errors.number && touched.number ? (
+              <small style={{ color: 'red' }}>{errors.number}</small>
+            ) : null}
           </div>
-
-          {errors.number && touched.number ? (
-            <small style={{ color: 'red' }}>{errors.number}</small>
-          ) : null}
-        </div>
-
-        <div style={{ marginBottom: '1.5rem' }}>
-          <label htmlFor="password">Password</label>
-
-          <div className="auth-input password-input">
-            <i className="fa-solid fa-lock"></i>
-            <input
-              value={values.password}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              id="password"
-              placeholder="Password (> 3 characters)"
-              autoComplete="off"
-              type={showPassword ? 'text' : 'password'}
-              name="password"
-            />
-
-            <i
-              onClick={() => setShowPassword(!showPassword)}
-              className={
-                showPassword
-                  ? 'fa-solid fa-eye'
-                  : 'fa-solid fa-eye-slash' + ' password'
-              }
-            ></i>
+  
+          <div style={{ marginBottom: '1.5rem' }}>
+            <label htmlFor="password">Password</label>
+  
+            <div className="auth-input password-input">
+              <i className="fa-solid fa-lock"></i>
+              <input
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                id="password"
+                placeholder="Password (> 3 characters)"
+                autoComplete="off"
+                type={showPassword ? 'text' : 'password'}
+                name="password"
+              />
+  
+              <i
+                onClick={() => setShowPassword(!showPassword)}
+                className={
+                  showPassword
+                    ? 'fa-solid fa-eye'
+                    : 'fa-solid fa-eye-slash' + ' password'
+                }
+              ></i>
+            </div>
+  
+            {errors.password && touched.password ? (
+              <small style={{ color: 'red' }}>{errors.password}</small>
+            ) : null}
           </div>
+  
+          <Link className="forgotPassword" to="/forgot-password">
+            Forgot Password?
+          </Link>
+  
+          <button type="submit" className="auth-btn">
+            Login
+          </button>
+  
+          <p className="auth-other-link">
+            Don't have an account? <Link to="/register">Register</Link>
+          </p>
+        </form>
+  
+        <ToastContainer
+          position="top-center"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+      </div>
+    );
+  }
 
-          {errors.password && touched.password ? (
-            <small style={{ color: 'red' }}>{errors.password}</small>
-          ) : null}
-        </div>
-
-        <Link className="forgotPassword" to="/forgot-password">
-          Forgot Password?
-        </Link>
-
-        <button type="submit" className="auth-btn">
-          Login
-        </button>
-
-        <p className="auth-other-link">
-          Don't have an account? <Link to="/register">Register</Link>
-        </p>
-      </form>
-
-      <ToastContainer
-        position="top-center"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-    </div>
-  );
+  
 };
 
 export default Login;
